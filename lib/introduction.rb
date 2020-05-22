6025a7b2dae2efc46dc3b353f025ee26277e8180
Melissa Sperry <sperry.mel@gmail.com>
2020-05-22 19:12:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,7 @@ def introduction (name)
 end
 
 def introduction_with_language (name, language)
+  name = "Dan"
+  language = "Ember.js"
   puts "Hi, my name is #{name} and I am learning #{language}."
 end
